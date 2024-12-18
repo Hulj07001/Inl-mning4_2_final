87e2f315632df3332518ed9a37f330a45f3715f3
Hugo Ljungqvist <hulj07001@utb.vaxjo.se>
2024-12-18 12:18:04 +0100

    Add project files.

---
+++ Inlämning4_2_final/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace inlämning4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int tal;
+                int störst = 0; //Måste ha ett värde för att kunna jämföras och bytas ut.
+
+
+                string svar = null;
+                do
+                {
+                    Console.WriteLine("Skriv in ett heltal");
+                    tal = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Vill du fortsätta? j/n.");
+                    svar = Console.ReadLine().ToLower();
+
+                    if (tal >= störst) //if satsen gör att om det givna "tal" är större än det nuvarande "störst" så byts "störst" ut med "tal".
+                    {
+                        störst = tal;
+                    }
+
+                } while (svar == "j");
+                {
+                    Console.WriteLine($"Det största talet du gav var: {störst}."); // $ dollatecknet gör att man kan ha en variabel inne i ett "".
+                    Console.WriteLine("Avslutar programmet");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("OBS! Fel vid inmatning."); // Vid felinmatning så visas detta meddelande.
+            }
+        }
+    }
+}
